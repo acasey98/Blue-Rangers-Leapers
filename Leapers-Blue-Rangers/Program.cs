a1363156cadb3ffef417d8c6d9c40fc81445942e
Austin Casey <thepoast@gmail.com>
2019-09-17 20:09:49 -0500

    first commit

---
+++ Leapers-Blue-Rangers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Leapers_Blue_Rangers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
