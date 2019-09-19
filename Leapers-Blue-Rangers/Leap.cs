6a01adb4ccf68755bc945e47e384fe661e7f4679
Calvin Foster <cal.j.foster@gmail.com>
2019-09-18 21:24:17 -0500

    leap class created

---
+++ Leapers-Blue-Rangers/Leap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leapers_Blue_Rangers
+{
+    class Leap
+    {
+        public int EventID;
+        public int LeaperID;
+        public Leap()
+        {
+            
+        }
+    }
+}
