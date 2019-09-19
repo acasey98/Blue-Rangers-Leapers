3dbd3e137343896c713bde56756bce7c48d55363
David Mullins <Mullins6323@gmail.com>
2019-09-18 21:07:49 -0500

    events and generateRandomEvent() created

--- Leapers-Blue-Rangers/Program.cs
+++ Leapers-Blue-Rangers/Program.cs
@@ -8,8 +8,60 @@ namespace Leapers_Blue_Rangers
     {
         static void Main(string[] args)
         {
+            var Events = new List<Event>()
+            {
+                new Event
+                {
+                    ID = 0,
+                    DateTime = new DateTime(2016, 11, 08),
+                    Description = "Trump inauguration",
+                    Hosts = new Dictionary<int, bool>
+                    {
+                        { 5, false },
+                        { 6, false },
+                        { 7, false }
+                    },
+                    isPutRight = false
+                },
+                new Event
+                {
+                    ID = 1,
+                    DateTime = new DateTime(1998, 08, 17),
+                    Description = "Clinton testimony",
+                    Hosts = new Dictionary<int, bool>
+                    {
+                        { 5, false },
+                        { 6, false },
+                        { 7, false }
+                    },
+                    isPutRight = false
 
-            var Hosts = new List<Host>();
+                },
+                new Event
+                {
+                    ID = 2,
+                    DateTime = new DateTime(2009, 09, 13),
+                    Description = "Kanye interrupts T-Swift VMA speech",
+                    Hosts = new Dictionary<int, bool>
+                    {
+                        { 5, false },
+                        { 6, false },
+                        { 7, false }
+                    },
+                    isPutRight = false
+                }
+            };
+
+            int RandomNumber(int min, int max)
+            {
+                Random random = new Random();
+                return random.Next(min, max);
+            }
+
+            int generateRandomEvent()
+            {
+                return RandomNumber(0, Events.Count);
+            }
         }
     }
 }
