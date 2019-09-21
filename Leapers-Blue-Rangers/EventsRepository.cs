fa4e3f06b46e1b7d5410376dda9d6cfb05f9430e
David Mullins <Mullins6323@gmail.com>
2019-09-21 10:14:43 -0500

    Removed merging comments

--- Leapers-Blue-Rangers/EventsRepository.cs
+++ Leapers-Blue-Rangers/EventsRepository.cs
@@ -53,5 +53,17 @@ namespace Leapers_Blue_Rangers
         {
             return _events;
         }
+
+        int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        int generateRandomEvent()
+        {
+            return RandomNumber(0, _events.Count);
+        }
+
     }
 }
