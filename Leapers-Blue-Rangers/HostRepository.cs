1097d6d2dcf4bffaa8caa203b754ecef01fcf86a
Calvin Foster <cal.j.foster@gmail.com>
2019-09-21 11:16:29 -0500

    new hostRepository

---
+++ Leapers-Blue-Rangers/HostRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leapers_Blue_Rangers
+{
+    class HostRepository
+    {
+        static List<Host> hosts = new List<Host>()
+        {
+             
+                new Host
+                {
+                    ID = 0,
+                    Name = "Cigar"
+                },
+                new Host
+                {
+                    ID = 1,
+                    Name = "Bill Clinton"
+                },
+                new Host
+                {
+                    ID = 2,
+                    Name = "Monica Lewinsky"
+                },
+                new Host
+                {
+                    ID = 3,
+                    Name = "Kanye West"
+                },
+                new Host
+                {
+                    ID = 4,
+                    Name = "VMA Judge"
+                },
+                new Host
+                {
+                    ID = 5,
+                    Name = "Voter"
+                },
+                new Host
+                {
+                    ID = 6,
+                    Name = "Donald Trump"
+                },
+                new Host
+                {
+                    ID = 7,
+                    Name = "Vladmir Putin"
+                }
+        };
+    }
+}
+
